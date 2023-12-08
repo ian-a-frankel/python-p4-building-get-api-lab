96ea432599ced6926b57161597190f3c74999883
Ian Frankel <ian.frankel@gmail.com>
2023-12-08 14:54:28 -0500

    gracefully routed baked goods

--- server/app.py
+++ server/app.py
@@ -20,18 +20,28 @@ def index():
 
 @app.route('/bakeries')
 def bakeries():
-    return ''
+    return make_response([bakery.to_dict() for bakery in Bakery.query.all()], 200)
 
 @app.route('/bakeries/<int:id>')
 def bakery_by_id(id):
-    return ''
+    bakery = db.session.get(Bakery, id)
+    print(bakery)
+    return make_response(jsonify(bakery.to_dict()), 200)
 
 @app.route('/baked_goods/by_price')
 def baked_goods_by_price():
-    return ''
+
+    goods = BakedGood.query.all()
+    sorted_goods = sorted(goods,key = lambda good: 1/good.price)
+    return make_response([good.to_dict() for good in sorted_goods],200)
 
 @app.route('/baked_goods/most_expensive')
 def most_expensive_baked_good():
+
+    goods = BakedGood.query.all()
+    priciest = sorted(goods,key = lambda good: 1/good.price)[0]
+    return make_response(priciest.to_dict(),200)
+
     return ''
 
 if __name__ == '__main__':
